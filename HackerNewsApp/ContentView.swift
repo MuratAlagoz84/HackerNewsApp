2b5c32a8c0700f5da19f68a31e3eff00e1c37c56
Murat Alagöz <muratalagoz84@gmail.com>
2019-10-17 12:29:25 +0300

    Initial Commit

---
+++ HackerNewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNewsApp
+//
+//  Created by Murat Alagöz on 17.10.2019.
+//  Copyright © 2019 Murat Alagöz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
